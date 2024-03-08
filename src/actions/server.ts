7c6ccf13a61729258f901f238ba9811c417f2480
Adam Davies <acdavies001@gmail.com>
2024-03-07 20:25:05 -0600

    Add getToken server helper

--- src/actions/server.ts
+++ src/actions/server.ts
@@ -3,7 +3,12 @@
 import { cookies } from 'next/headers';
 
 import type { SideType } from '@/store';
+import { AccessToken } from '@/app/api/token/route';
 
+export async function getToken(side: SideType) {
+  const cookieStore = cookies();
+  const cookie = cookieStore.get(`token-${side}`);
+  const token = cookie && (JSON.parse(cookie.value) as AccessToken);
 
   return token;
 }
