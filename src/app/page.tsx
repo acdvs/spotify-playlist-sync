2be8f6f1bdabced0566578766e9780c65bbeadb6
Adam Davies <acdavies001@gmail.com>
2024-09-01 01:23:54 -0500

    Clean up unused logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { getToken } from '@/actions/server';
 import Header from '@/components/Header';
 import Side from '@/components/side/Side';
 import SyncButton from '@/components/buttons/Sync';
 
 export default async function Index() {
-  const tokenLeft = await getToken('left');
-  const tokenRight = await getToken('right');
-
   return (
     <div className="lg:w-[900px] p-6 h-full lg:h-auto">
       <Header />
