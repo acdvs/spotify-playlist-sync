df1b59afcc08c6610beb8f2a4a367118d8bafd9d
Adam Davies <acdavies001@gmail.com>
2024-09-01 19:53:44 -0500

    Move help text to dialog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
 import { RiArrowLeftRightLine, RiQuestionLine } from '@remixicon/react';
 
 import Tooltip from './Tooltip';
+import Card from './Card';
 
 const Header = () => {
   const [helpActive, showHelp] = useState(false);
-  const helpRef = useRef<HTMLParagraphElement>(null);
-
-  const helpStyles = {
-    marginTop: helpActive ? '0px' : `calc(-${helpRef.current?.clientHeight}px - 0.75rem)`,
-    opacity: helpActive ? 1 : 0,
-  };
 
   return (
     <div className="mb-10">
@@ -29,11 +25,32 @@ const Header = () => {
           />
         </Tooltip>
       </div>
-      <p className="transition-all" style={helpStyles} ref={helpRef}>
-        First, log into two different Spotify accounts below. Then, simply select a
-        playlist from each account and press the sync button. The playlist on the left
-        will be synced to the playlist on the right.
-      </p>
+      <Dialog
+        open={helpActive}
+        onClose={() => showHelp(false)}
+        transition
+        className="transition duration-300 ease-out data-[closed]:opacity-0"
+      >
+        <DialogBackdrop className="fixed inset-0 bg-black/30" />
+        <div className="w-96 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <DialogPanel>
+            <Card filled noAlign className="gap-5">
+              <DialogTitle className="font-bold">Directions</DialogTitle>
+              <p>
+                First, log into two different Spotify accounts below. Then, simply select
+                a playlist from each account and press the sync button.
+              </p>
+              <p>
+                The playlist on the left will be synced to the playlist on the right. This
+                can be flipped with the button in the top right corner.
+              </p>
+              <div onClick={() => showHelp(false)} className="button primary self-start">
+                Got it
+              </div>
+            </Card>
+          </DialogPanel>
+        </div>
+      </Dialog>
     </div>
   );
 };
