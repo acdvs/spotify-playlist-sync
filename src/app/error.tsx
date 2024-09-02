ffe8df93ab3bf21add396dd11ef6276f963465a4
Adam Davies <acdavies001@gmail.com>
2024-09-02 00:49:59 -0500

    Improve error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,13 +4,16 @@ import Card from '@/components/Card';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
-    <Card text="Oops! Something went wrong..." className="max-w-[400px]">
+    <Card
+      text="Oops! Something went wrong..."
+      className="w-[400px] max-w-[95vw] self-center"
+    >
       <div className="flex self-start mb-5">
         <p className="mr-3">Error:</p>
         <p>{error.message}</p>
       </div>
       <div className="button primary" onClick={reset}>
-        <p>Try again</p>
+        <p>Go back</p>
       </div>
     </Card>
   );
