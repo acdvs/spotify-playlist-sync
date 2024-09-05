7dd44b4a37174785fbe4c375e91635e1d3c78a40
Adam Davies <acdavies001@gmail.com>
2024-09-05 05:15:08 -0500

    Abstract show help button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useState } from 'react';
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
-import { RiArrowLeftRightLine, RiQuestionLine } from '@remixicon/react';
+import { RiArrowLeftRightLine } from '@remixicon/react';
 
 import Tooltip from './Tooltip';
-import Card from './Card';
+import ShowHelp from './buttons/ShowHelp';
 
 const Header = () => {
-  const [helpActive, showHelp] = useState(false);
 
   return (
     <>
@@ -24,33 +21,8 @@ const Header = () => {
             onClick={() => showHelp((x) => !x)}
           />
         </Tooltip>
+        <ShowHelp />
       </div>
-      <Dialog
-        open={helpActive}
-        onClose={() => showHelp(false)}
-        transition
-        className="transition duration-300 ease-out data-[closed]:opacity-0"
-      >
-        <DialogBackdrop className="fixed inset-0 bg-black/70" />
-        <div className="w-96 max-w-[95vw] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <DialogPanel>
-            <Card filled noAlign className="gap-5 p-5">
-              <DialogTitle className="font-bold">Directions</DialogTitle>
-              <p>
-                Log into two different Spotify accounts, select a playlist from each
-                account, and press the sync button.
-              </p>
-              <p>
-                The first playlist will be synced to the second playlist. This can be
-                flipped with the button in the top right corner.
-              </p>
-              <div onClick={() => showHelp(false)} className="button primary self-center">
-                Got it
-              </div>
-            </Card>
-          </DialogPanel>
-        </div>
-      </Dialog>
     </>
   );
 };
