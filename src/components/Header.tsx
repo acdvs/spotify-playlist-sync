6d9d51699a2b6370a6e2f8fef3b2f455e6a75bcc
Adam Davies <acdavies001@gmail.com>
2024-09-05 12:28:20 -0500

    Simplify Header structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,13 @@
 import FlipSync from './buttons/FlipSync';
 import ShowHelp from './buttons/ShowHelp';
 
-const Header = () => {
-  return (
-    <>
-      <div className="flex gap-5 items-center">
-        <h1 className="flex-shrink-0">Spotify Playlist Sync</h1>
-        <div className="border-zinc-700 border-b-2 w-full" />
-        <FlipSync />
-        <ShowHelp />
-      </div>
-    </>
-  );
-};
+const Header = () => (
+  <div className="flex gap-5 items-center">
+    <h1 className="flex-shrink-0">Spotify Playlist Sync</h1>
+    <div className="border-zinc-700 border-b-2 w-full" />
+    <FlipSync />
+    <ShowHelp />
+  </div>
+);
 
 export default Header;
