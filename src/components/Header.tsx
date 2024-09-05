c42e8bb13e16b97803c45ac560a1c11c52fd294d
Adam Davies <acdavies001@gmail.com>
2024-09-05 04:58:56 -0500

    Darken dialog backdrop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
         transition
         className="transition duration-300 ease-out data-[closed]:opacity-0"
       >
-        <DialogBackdrop className="fixed inset-0 bg-black/30" />
+        <DialogBackdrop className="fixed inset-0 bg-black/70" />
         <div className="w-96 max-w-[95vw] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <DialogPanel>
             <Card filled noAlign className="gap-5 p-5">
